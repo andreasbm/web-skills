7e29655ddb830b4d92e8793cc10e4d8eb63dd6c5
andreasbm <andmehlsen@gmail.com>
2019-10-05 16:01:33 +0200

    Finished team collaboration.

--- data/team-collaboration.js
+++ data/team-collaboration.js
@@ -3,21 +3,49 @@ export const teamCollaborationCollection = {
 	areas: [
 		{
 			name: "Version Control",
+			description: {
+				text: "Learn about version control and why it is used when creating software in teams.",
+				links: [
+					["A Visual Guide to Version Control", "https://betterexplained.com/articles/a-visual-guide-to-version-control/"],
+					["Getting Started - About Version Control", "https://git-scm.com/book/en/v2/Getting-Started-About-Version-Control"],
+					["What is version control", "https://www.atlassian.com/git/tutorials/what-is-version-control"],
+				]
+			},
 			skills: [
 				{
 					name: "Git",
 					description: {
-						text: "",
+						text: "Learn about git and how it is used for version control.",
 						links: [
-							["Learn Git Branching", "https://learngitbranching.js.org/"]
+							["Learn Git Branching", "https://learngitbranching.js.org/"],
+							["Git Handbook", "https://guides.github.com/introduction/git-handbook/"],
+							["Github Cheatsheet", "https://github.github.com/training-kit/downloads/github-git-cheat-sheet/"],
+							["Visualizing Git", "http://git-school.github.io/visualizing-git/"],
+							["Codecademy - Learn Gitthis", "https://www.codecademy.com/learn/learn-git"],
 						]
 					},
 					skills: [
 						{
-							name: "Github"
+							name: "Github",
+							description: {
+								text: "Learn how to use Github.",
+								links: [
+									["What is GitHub?", "https://guides.github.com/activities/hello-world/"],
+									["GitHub Learning Lab", "https://lab.github.com/"],
+									["Wikipedia - Github", "https://en.wikipedia.org/wiki/Github"],
+								]
+							},
 						},
 						{
-							name: "Bitbucket"
+							name: "Bitbucket",
+							description: {
+								text: "Learn how to use Bitbucket.",
+								links: [
+									["Wikipedia - Bitbucket", "https://en.wikipedia.org/wiki/Bitbucket"],
+									["Tutorial: Learn Bitbucket with Git", "https://confluence.atlassian.com/bitbucket/tutorial-learn-bitbucket-with-git-759857287.html"],
+									["Getting Started with Bitbucket for Version Control", "https://www.tecmint.com/bitbucket-for-version-control/"],
+								]
+							},
 						}
 					]
 				}
@@ -25,20 +53,58 @@ export const teamCollaborationCollection = {
 		},
 		{
 			name: "Management",
+			description: {
+				text: "Learn about software development management and get an overview of the various methodologies.",
+				links: [
+					["Software Project Management", "https://www.tutorialspoint.com/software_engineering/software_project_management.htm"],
+					["Wikipedia - Software project management", "https://en.wikipedia.org/wiki/Software_project_management"],
+				]
+			},
 			skills: [
 				{
 					name: "Agile Development",
+					description: {
+						text: "Learn about agile development and when to use it.",
+						links: [
+							["Wikipedia - Agile software development", "https://en.wikipedia.org/wiki/Agile_software_development"],
+							["What is agile methodology?", "https://www.infoworld.com/article/3237508/what-is-agile-methodology-modern-software-development-explained.html"],
+							["What is Agile Software Development?", "https://www.visual-paradigm.com/scrum/what-is-agile-software-development/"],
+						]
+					},
 					skills: [
 						{
-							name: "Scrum"
+							name: "Scrum",
+							description: {
+								text: "Learn about scrum and when to use it.",
+								links: [
+									["Scrum Tutorial", "https://www.knowledgehut.com/tutorials/scrum-tutorial"],
+									["Scrum - Overview", "https://www.tutorialspoint.com/scrum/scrum_overview.htm"],
+								]
+							},
 						},
 						{
-							name: "Kanban"
+							name: "Kanban",
+							description: {
+								text: "Learn about kanban and when to use it.",
+								links: [
+									["Kanban Tutorial", "https://www.tutorialspoint.com/kanban/kanban_introduction.htm"],
+									["What is kanban?", "https://www.atlassian.com/agile/kanban"],
+									["Kanban Fundamentals", "https://www.scrumhub.com/kanban-fundamentals/"],
+								]
+							},
 						},
 					]
 				},
 				{
-					name: "Waterfall Development"
+					name: "Waterfall Development",
+					description: {
+						text: "Learn about waterfall development and when to use it.",
+						links: [
+							["Waterfall Model", "https://www.tutorialspoint.com/sdlc/sdlc_waterfall_model.htm"],
+							["Waterfall vs. Agile", "https://www.seguetech.com/waterfall-vs-agile-methodology/"],
+							["Waterfall Model: What and when", "https://airbrake.io/blog/sdlc/waterfall-model"],
+						]
+					},
 				}
 			]
 		},
