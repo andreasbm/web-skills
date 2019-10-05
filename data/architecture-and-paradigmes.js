9a911f070c2d876e789994caa7234817571c3eb5
andreasbm <andmehlsen@gmail.com>
2019-10-05 15:44:34 +0200

    Finished architecture.

--- data/architecture-and-paradigmes.js
+++ data/architecture-and-paradigmes.js
@@ -4,51 +4,199 @@ export const architectureCollection = {
 		{
 			skills: [
 				{
-					name: "Functional programming",
+					name: "Programming Paradigmes",
+					description: {
+						text: "Learn how programming languages can be categoried into different paraigmes based on their features.",
+						links: [
+							["Wikipedia - Programming paradigm", "https://en.wikipedia.org/wiki/Programming_paradigm"],
+							["What Are JavaScript Programming Paradigms?", "https://medium.com/javascript-in-plain-english/what-are-javascript-programming-paradigms-3ef0f576dfdb"],
+						]
+					},
 					skills: [
 						{
-							name: "Recursion"
-						}
-					]
-				},
-				{
-					name: "Object Oriented Programming",
-					skills: [
+							name: "Functional programming",
+							description: {
+								text: "Learn about functional programming and how it can make your code easier to read and debug.",
+								links: [
+									["An introduction to functional programming in JavaScript", "https://opensource.com/article/17/6/functional-javascript"],
+									["Functional Programming in JavaScript", "https://codeburst.io/functional-programming-in-javascript-e57e7e28c0e5"],
+									["The power of functional programming in Javascript", "https://itnext.io/the-power-of-functional-programming-in-javascript-cc9797a42b60"],
+									["An introduction to Functional Programming", "https://flaviocopes.com/javascript-functional-programming/"],
+									["dev.to - Basic Functional Programming Patterns in JavaScript", "https://dev.to/nestedsoftware/basic-functional-programming-patterns-in-javascript-49p2"],
+								]
+							},
+							skills: [
+								{
+									name: "Recursion",
+									description: {
+										text: "Learn about recursion and when it should be used to make your code more readable.",
+										links: [
+											["Learn and Understand Recursion in JavaScript", "https://codeburst.io/learn-and-understand-recursion-in-javascript-b588218e87ea"],
+											["Understanding Recursion in JavaScript", "https://medium.com/@zfrisch/understanding-recursion-in-javascript-992e96449e03"],
+											["Freecodecamp - Recursion in JavaScript", "https://www.freecodecamp.org/news/recursion-in-javascript-1608032c7a1f/"],
+											["javascript.info - Recursion and stack", "https://javascript.info/recursion"],
+										]
+									},
+								}
+							]
+						},
 						{
-							name: "S.O.L.I.D"
-						}
+							name: "Object Oriented Programming",
+							description: {
+								text: "Learn about object oriented programming and how objects can make your code easier to read and debug.",
+								links: [
+									["Mozilla - Object-oriented JavaScript", "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object-oriented_JS"],
+									["Freecodecamp - An introduction to Object-Oriented Programming", "https://www.freecodecamp.org/news/an-introduction-to-object-oriented-programming-in-javascript-8900124e316a/"],
+									["Introduction to Object Oriented Programming in JavaScript", "https://www.geeksforgeeks.org/introduction-object-oriented-programming-javascript/"],
+								]
+							},
+							skills: [
+								{
+									name: "S.O.L.I.D",
+									description: {
+										text: "Learn about the SOLID design pricinples that can make your code more understandable, flexible and maintainable.",
+										links: [
+											["Wikipedia - SOLID", "https://en.wikipedia.org/wiki/SOLID"],
+											["S.O.L.I.D: The First 5 Principles of Object Oriented Design", "https://scotch.io/bar-talk/s-o-l-i-d-the-first-five-principles-of-object-oriented-design"],
+											["S.O.L.I.D The first 5 principles of Object Oriented Design with JavaScript", "https://medium.com/@cramirez92/s-o-l-i-d-the-first-5-priciples-of-object-oriented-design-with-javascript-790f6ac9b9fa"],
+										]
+									},
+								}
+							]
+						},
 					]
 				},
 				{
 					name: "Design Patterns",
+					description: {
+						text: "Learn how commonly occurring problems can be solved using design patterns.",
+						links: [
+							["Wikipedia - Software design pattern", "https://en.wikipedia.org/wiki/Software_design_pattern"],
+							["The 7 Most Important Software Design Patterns", "https://medium.com/educative/the-7-most-important-software-design-patterns-d60e546afb0e"],
+							["Software Design Patterns", "https://www.geeksforgeeks.org/software-design-patterns/"],
+							["Design Patterns", "https://refactoring.guru/design-patterns"],
+							["JavaScript Design Patterns", "https://www.dofactory.com/javascript/design-patterns"],
+						]
+					},
 					skills: [
 						{
-							name: "Singleton"
+							name: "Singleton",
+							description: {
+								text: "Learn about the singleton design pattern and when it should be used.",
+								links: [
+									["Singleton Pattern", "https://refactoring.guru/design-patterns/singleton"],
+									["Singleton Example", "https://www.dofactory.com/javascript/singleton-design-pattern"],
+								]
+							},
 						},
 						{
-							name: "Observer"
+							name: "Observer",
+							description: {
+								text: "Learn about the observer design pattern and when it should be used.",
+								links: [
+									["Observer Pattern", "https://refactoring.guru/design-patterns/observer"],
+									["Observer Example", "https://www.dofactory.com/javascript/observer-design-pattern"],
+								]
+							},
 						},
 						{
-							name: "Prototype"
+							name: "Prototype",
+							description: {
+								text: "Learn about the prototype design pattern and when it should be used.",
+								links: [
+									["Prototype Pattern", "https://refactoring.guru/design-patterns/prototype"],
+									["Prototype Example", "https://www.dofactory.com/javascript/prototype-design-pattern"],
+								]
+							},
 						},
 						{
-							name: "Bridge"
+							name: "Bridge",
+							description: {
+								text: "Learn about the bridge design pattern and when it should be used.",
+								links: [
+									["Bridge Pattern", "https://refactoring.guru/design-patterns/bridge"],
+									["Bridge Example", "https://www.dofactory.com/javascript/bridge-design-pattern"],
+								]
+							},
 						},
 						{
-							name: "Proxy"
+							name: "Proxy",
+							description: {
+								text: "Learn about the proxy design pattern and when it should be used.",
+								links: [
+									["Proxy Pattern", "https://refactoring.guru/design-patterns/proxy"],
+									["Proxy Example", "https://www.dofactory.com/javascript/proxy-design-pattern"],
+								]
+							},
 						},
 						{
-							name: "Chain of responsibility"
+							name: "Chain of responsibility",
+							description: {
+								text: "Learn about the chain of responsibility design pattern and when it should be used.",
+								links: [
+									["Chain of Responsibility Pattern", "https://refactoring.guru/design-patterns/chain-of-responsibility"],
+									["Chain of Responsibility Example", "https://www.dofactory.com/javascript/chain-of-responsibility-design-pattern"],
+								]
+							},
 						},
 						{
-							name: "Constructor"
+							name: "Constructor",
+							description: {
+								text: "Learn about the constructor design pattern and when it should be used.",
+								links: [
+									["The Constructor Pattern", "https://www.oreilly.com/library/view/learning-javascript-design/9781449334840/ch09s01.html"],
+									["Fundamental Object Design Patterns in JavaScript", "https://medium.com/@severinperez/javascript-weekly-fundamental-object-design-patterns-31453f68427f"],
+									["Difference between Constructor pattern and Prototype pattern", "https://stackoverflow.com/questions/35057827/difference-between-constructor-pattern-and-prototype-pattern"],
+								]
+							},
 						}
 					]
 				},
 				{
-					name: "BEM Pattern"
-				},
-
+					name: "CSS Methodologies",
+					description: {
+						text: "Learn how CSS can be structured using CSS methodologies.",
+						links: [
+							["A Look at Some CSS Methodologies", "https://www.webfx.com/blog/web-design/css-methodologies/"],
+							["A web designer's guide to CSS methodologies", "https://www.creativebloq.com/features/a-web-designers-guide-to-css-methodologies"],
+							["Understanding CSS Writing Methodologies", "https://www.hongkiat.com/blog/css-writing-methodologies/"],
+						]
+					},
+					skills: [
+						{
+							name: "BEM",
+							description: {
+								text: "Learn about the BEM methodology and when it should be used.",
+								links: [
+									["Get BEM", "http://getbem.com/"],
+									["CSS Tricks - BEM 101", "https://css-tricks.com/bem-101/"],
+									["What is BEM? And Why you should use it in your project.", "https://medium.com/@dannyhuang_75970/what-is-bem-and-why-you-should-use-it-in-your-project-ab37c6d10b79"],
+								]
+							},
+						},
+						{
+							name: "SMACSS",
+							description: {
+								text: "Learn about the SMACSS methodology and when it should be used.",
+								links: [
+									["SMACSS", "http://smacss.com/"],
+									["BEM vs SMACSS", "https://www.sitepoint.com/bem-smacss-advice-from-developers/"],
+								]
+							},
+						},
+						{
+							name: "OOCSS",
+							description: {
+								text: "Learn about the OOCSS methodology and when it should be used.",
+								links: [
+									["OOCSS", "https://github.com/stubbornella/oocss/wiki"],
+									["An Introduction To Object Oriented CSS", "https://www.smashingmagazine.com/2011/12/an-introduction-to-object-oriented-css-oocss/"],
+									["The Future of Writing CSS", "https://www.keycdn.com/blog/oocss"],
+								]
+							},
+						}
+					]
+				}
 			]
 		}
 	]
