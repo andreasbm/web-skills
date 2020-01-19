0244befce972f8e615cc0a16392bcdefc57aeb7e
andreasbm <andmehlsen@gmail.com>
2020-01-20 00:51:15 +0100

    Add lazy loading to skill elements.

--- config.js
+++ config.js
@@ -9,3 +9,9 @@ export const firebaseConfig = {
 	messagingSenderId: "796050122198",
 	appId: "1:796050122198:web:422fd7fee1037c3a2a3077"
 };
+
+export const lazyImgIntersectionObserverOptions = {
+	rootMargin: '200px',
+	// [0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09]
+	threshold: Array(10).fill(0).map((_, i) => (i * 1) / 100)
+}
--- util/util.js
+++ util/util.js
@@ -1,12 +1,7 @@
 import { Confetti } from "../atoms/confetti.js";
+import {lazyImgIntersectionObserverOptions} from "./../config.js";
 
 const AUDIO_CACHE = new Map();
-const lazyImgIntersectionObserverOptions = {
-	rootMargin: '200px',
-	// [0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09]
-	threshold: Array(10).fill(0).map((_, i) => (i * 1) / 100)
-}
-
 /**
  * Turns a skill name into a path.
  * @param {*} name 
