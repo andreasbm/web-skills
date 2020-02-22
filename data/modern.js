60a2bf529d937285d248ced1c36022bbf4a9aad9
andreasbm <andmehlsen@gmail.com>
2020-02-22 18:21:27 +0100

    Fix modern json.

--- data/modern.js
+++ data/modern.js
@@ -569,7 +569,7 @@ export const modernCollection = {
 							["Medium - A Simple Guide to Understanding Javascript Generators", "https://medium.com/dailyjs/a-simple-guide-to-understanding-javascript-es6-generators-d1c350551950"],
 						]
 					},
-				}
+				},
 				{
 					name: "Proxies",
 					description: {
